4178dec8f9e646dc664e0c56a3314150109a5aae
birendramphil <birendramphil@gmail.com>
2024-06-26 21:02:11 +0545

    push by birendra

--- src/checktbl/tableframe-1.java
+++ src/checktbl/tableframe-1.java
@@ -35,7 +35,7 @@ public class tableframe extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));//this is new
 
 		setContentPane(contentPane);
 		
